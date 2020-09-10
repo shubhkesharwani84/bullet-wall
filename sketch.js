6cb858f68004e1e94a1c03e8c18d3a1b46ba9121
shubhkesharwani84 <70700864+shubhkesharwani84@users.noreply.github.com>
2020-09-10 18:24:13 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -9,12 +9,35 @@ function setup() {
   bullet = createSprite(50,200,80,20)
   bullet.velocityX = speed;
   wall = createSprite(1200,200,thickness,height/2)
-  
- 
-}
+  wall.shapeColor = color(80,80,80)
+ }
 
 function draw() {
   background(0,255,255);  
- 
+   
+  if(hascollided(bullet,wall)){
+    bullet.velocityX =0;
+  var damage =0.5*weight *speed * speed/(thickness*thickness*thickness);
+  
+  if(damage>10){
+    bullet.shapeColor = "red"
+  }
+  if(damage<10)
+  {bullet.shapeColor = "green"}
+  
+
+  }
+  
 drawSprites();
+}
+function hascollided(bullet,wall){
+bulletRightEdge = bullet.x = bullet.width;
+wallLeftEdge = wall.x
+
+if(bulletRightEdge>=wallLeftEdge){
+   return true;
+}else{
+  return false;
+}
+
 }
